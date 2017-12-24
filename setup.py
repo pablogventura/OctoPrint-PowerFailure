ae5fd48bce11056d772fc30f6b6421ba1cb31ca9
Pablo Ventura <pablo@claude.workgroup>
2017-12-23 21:23:10 -0300

    setup

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "octoprint_eta"
+plugin_identifier = "powerfailure"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_eta"
+plugin_package = "octoprint_powerfailure"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-Display-ETA"
+plugin_name = "Power Failure Recovery"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.0"
+plugin_version = "1.0.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """Show finish time (ETA time) for current print."""
+plugin_description = """Recovers a print after a power failure."""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Pablo Ventura"
@@ -27,7 +27,7 @@ plugin_author = "Pablo Ventura"
 plugin_author_email = "pablogventura@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/pablogventura/Octoprint-ETA"
+plugin_url = "https://github.com/pablogventura/Octoprint-PowerFailure"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
